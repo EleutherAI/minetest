3d96b240fb91e94ce3a645f4815e947d0c600e39
neverix <nev@neverix.io>
2022-12-02 13:36:52 +0300

    Update init.lua

--- mods/rewards/init.lua
+++ mods/rewards/init.lua
@@ -1,7 +1,21 @@
-minetest.register_globalstep(function()
-    for _, player in pairs(minetest.get_connected_players()) do
-        local hp = player:get_hp()
-        player:get_meta():set_float("reward", hp)
-        print(hp)
-    end
+reward_channels = {}
+
+function get_channel(player)
+    return "reward"..player:get_nametag_attributes().text
+end
+
+minetest.register_on_joinplayer(function(player, _)
+    local channel = get_channel(player)
+    reward_channels[channel] = minetest.mod_channel_join(channel)
 end)
+
+minetest.register_on_player_hpchange(function(player, hp_change, reason)
+    local channel = get_channel(player)
+    reward_channels[channel]:send_all(hp_change)
+end)
+
+-- minetest.register_globalstep(function()
+--     for _, player in pairs(minetest.get_connected_players()) do
+
+--     end
+-- end)
