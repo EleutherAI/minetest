1af954c639f3cc6a705e18158336f2661093f5e8
AI_WAIFU <aiwaifu@protonmail.com>
2023-07-17 01:38:00 +0100

    better data recorder

--- minetester/data_recorder.py
+++ minetester/data_recorder.py
@@ -1,7 +1,10 @@
 import os
+import time
+import json
+import cv2
 import zmq
+import numpy as np
 from minetester.utils import unpack_pb_obs
-
 
 class DataRecorder:
     def __init__(
@@ -12,15 +15,15 @@ class DataRecorder:
         max_queue_length: int = 1200,
         max_attempts: int = 10,
         debug: bool = False,
+        subsample_rate: int = 1,  # new argument for subsampling rate
     ):
         self.target_address = target_address
         self.data_path = data_path
         self.timeout = timeout
         self.max_queue_length = max_queue_length
         self.max_attempts = max_attempts
-        # Debug mode prints received actions and does not save to file
         self.debug = debug
-
+        self.subsample_rate = subsample_rate  # subsampling rate
         self._recording = False
 
         # Setup ZMQ
@@ -38,30 +41,48 @@ class DataRecorder:
         # Set timeout in milliseconds
         self.socket.setsockopt(zmq.RCVTIMEO, 1000)
 
+        # Create data directory if it doesn't exist
+        os.makedirs(self.data_path, exist_ok=True)
+
+        # Prepare paths for video and action log
+        self.video_path = os.path.join(self.data_path, "video.avi")
+        self.action_log_path = os.path.join(self.data_path, "actions.jsonl")
+
+        # Initialize video writer
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        self.video_writer = cv2.VideoWriter(self.video_path, self.fourcc, 20.0, (1024, 600))
+
     def start(self):
-        with open(self.data_path, "w") as out:
+        with open(self.action_log_path, "w") as action_log:
             self._recording = True
             num_attempts = 0
+            frame_count = 0  # count frames for subsampling
+
             while self._recording:
                 try:
                     # Receive data
                     raw_data = self.socket.recv()
                     num_attempts = 0
 
-                    if self.debug:
+                    # Subsample based on the specified rate
+                    if frame_count % self.subsample_rate == 0:
                         obs, rew, terminal, info, action = unpack_pb_obs(raw_data)
-                        print(obs, type(obs))
-                        action_str = ""
-                        for key in action.keys():
-                            if key != "MOUSE" and action[key]:
-                                action_str += key + ", "
-                        print(f"action={action_str}, rew={rew}, T?={terminal}")
 
-                    # Write data to new line
-                    if not self.debug:
-                        obs, rew, terminal, info, action = unpack_pb_obs(raw_data)
-                        print(obs, type(obs))
-                        out.write(str(raw_data) + "\n")
+                        if self.debug:
+                            print(obs, type(obs))
+                            action_str = ""
+                            for key in action.keys():
+                                if key != "MOUSE" and action[key]:
+                                    action_str += key + ", "
+                            print(f"action={action_str}, rew={rew}, T?={terminal}")
+
+                        # Write frame to video
+                        self.video_writer.write(cv2.cvtColor(np.array(obs), cv2.COLOR_RGB2BGR))
+
+                        # Write action to action log (one line per frame)
+                        action_log.write(json.dumps(action) + "\n")
+
+                    frame_count += 1
                 except zmq.ZMQError as err:
                     if err.errno == zmq.EAGAIN:
                         print(f"Reception attempts: {num_attempts}")
@@ -75,3 +96,5 @@ class DataRecorder:
 
     def stop(self):
         self._recording = False
+        self.video_writer.release()  # release video writer
+
